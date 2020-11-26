39c2c7f34d8f93a212316cbcb3ad5b1a23a8938f
Ryan Flynn <ryanflynn44@gmail.com>
2020-11-26 14:51:08 -0500

    Made counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,13 @@
 
 
-function Counter() {
+const Counter = () => {
 
     return (
         <div>
-
+            <h3>Welcome Ryan!</h3>
+            <h3>Counter :0</h3>
+            <button>increase</button>
+            <button>decrease</button>
         </div>
     )
 }
